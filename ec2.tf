28b7c43844559168a568ee8012d5bb6c0b7672a4
Suprajaa-27 <97280312+Suprajaa-27@users.noreply.github.com>
2023-11-03 12:00:03 +0100

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,20 @@
+resource "google_service_account" "default" {
+  account_id   = "my-custom-sa"
+  display_name = "Custom SA for VM Instance"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "my-instance"
+  machine_type = "e2-micro"
+  zone         = "us-west1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "root"
+      }
+    }
+  }
