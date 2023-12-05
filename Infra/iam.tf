22e60b7ece35233b6f2df1b7d7f48e36f51855f1
suprajaa-27 <suprajaa96@gmail.com>
2023-12-05 12:19:03 +0100

    lambda function creation

--- Infra/iam.tf
+++ Infra/iam.tf
@@ -1,6 +1,6 @@
 # Define an IAM role for the Lambda function
 resource "aws_iam_role" "aws_lambda_role" {
-  name = "example-lambda-role"
+  name = "aws-lambda-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -18,7 +18,7 @@ resource "aws_iam_role" "aws_lambda_role" {
 
 resource "aws_iam_policy" "iam_policy_for_lambda" {
 
-  name         = "aws_iam_policy_for_terraform_aws_lambda_role"
+  name         = "aws_iam_policy_for_aws_lambda_role"
   path         = "/"
   description  = "AWS IAM Policy for managing aws lambda role"
   policy = <<EOF
