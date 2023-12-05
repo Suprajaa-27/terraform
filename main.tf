496c8b67619c29b14a52c08a314f6f65b09f76d2
suprajaa-27 <suprajaa96@gmail.com>
2023-12-05 11:41:21 +0100

    updated role for lambda.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "infra" {
     source = "./Infra"
     bucket = var.bucket
     region = var.region
-    lambda_iam_role_arn = outputs.lambda_iam_role_arn 
+    lambda_iam_role_arn = output.lambda_iam_role_arn 
 }
 
 
