26eea10422ca8f95309eabb8fa5ac8d9ee397045
suprajaa-27 <suprajaa96@gmail.com>
2023-12-05 08:25:52 +0100

    commented out lambda.tf

--- Infra/lambda.tf
+++ Infra/lambda.tf
@@ -1,7 +1,7 @@
-resource "aws_lambda_function" "s3_trigger_lambda" {
-  function_name = "s3_trigger_lambda"
-  handler      = "handler.handler"
-  runtime      = "python3.11"
-  role         = aws_iam_role.lambda_role.arn
-  filename     = "lambda-function.zip"  # Replace with the path to your Lambda deployment package
-}
+# resource "aws_lambda_function" "s3_trigger_lambda" {
+#   function_name = "s3_trigger_lambda"
+#   handler      = "handler.handler"
+#   runtime      = "python3.11"
+#   role         = aws_iam_role.lambda_role.arn
+#   filename     = "lambda-function.zip"  # Replace with the path to your Lambda deployment package
+# }
