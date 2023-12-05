06067e4b26b08a1b2cc7ef0117e7131ca271d5be
suprajaa-27 <suprajaa96@gmail.com>
2023-12-05 20:53:45 +0100

    code update

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ module "lambda_s3_trigger" {
   lambda_function_name  = var.function_name
   handler        = var.handler
   runtime        = var.runtime
-  role_arn       = aws_iam_role.aws_lambda_role.arn
+  role_arn       = module.lambda_s3_trigger.lambda_role_arn
   filename       = "${path.module}/lambda_function.zip"
-  s3_bucket_id   = s3_bucket.s3_bucket_id
+  s3_bucket_id   = module.lambda_s3_trigger.s3_bucket_id
   trigger_events = var.trigger_events
   filter_prefix  = var.filter_prefix
   filter_suffix  = var.filter_suffix
--- modules/lambda_s3_trigger/outputs.tf
+++ modules/lambda_s3_trigger/outputs.tf
@@ -12,10 +12,15 @@ output "s3_trigger_id" {
 
 output "s3_bucket_id" {
   description = "The ID of s3 bucket."
-  value = aws_s3_bucket.s3_bucket.id
+  value       = aws_s3_bucket.s3_bucket.id
 }
 
 output "s3_bucket_arn" {
   description = "The arn of s3 bucket."
-  value = aws_s3_bucket.s3_bucket.arn
+  value       = aws_s3_bucket.s3_bucket.arn
+}
+
+output "lambda_role_arn" {
+  description = "The arn of lambda role"
+  value       = aws_iam_role.aws_lambda_role.arn
 }
