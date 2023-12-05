9a7f12d457284d10cfcf1351ec417f06da84e84e
suprajaa-27 <suprajaa96@gmail.com>
2023-12-05 11:25:14 +0100

    updated iam.tf bucket arn

--- Infra/outputs.tf
+++ Infra/outputs.tf
@@ -1,3 +1,8 @@
 output "s3_bucket_arn" {
-    value = resource.aws_s3_bucket.s3_bucket.arn
+    value = aws_s3_bucket.s3_bucket.arn
+}
+
+output "lambda_function_name" {
+    value = aws_lambda_function.s3_trigger_lambda.name
+    description = "The name of the Lambda Function"
 }
