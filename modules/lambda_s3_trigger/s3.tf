61fd47576a7647dfb5db8137a5678a1cb86c0812
suprajaa-27 <suprajaa96@gmail.com>
2023-12-07 14:27:30 +0100

    Lifecyle rule for s3

--- modules/lambda_s3_trigger/s3.tf
+++ modules/lambda_s3_trigger/s3.tf
@@ -2,3 +2,30 @@
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
 }
+resource "aws_s3_bucket_lifecycle_configuration" "bucket-config" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  rule {
+    id = "object-lifecycle-rule"
+
+    expiration {
+      days = 90
+    }
+    status = "Enabled"
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days          = 60
+      storage_class = "GLACIER"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
